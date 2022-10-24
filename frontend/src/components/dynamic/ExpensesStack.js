4f63002d7c0e72bcc2a30ebebabfa60f8d28ef80
Kiril Ivanov <kiril.ivanov91@gmail.com>
2022-10-24 22:55:20 +0300

    Added Savings feature to front-end

--- frontend/src/components/dynamic/ExpensesStack.js
+++ frontend/src/components/dynamic/ExpensesStack.js
@@ -1,26 +1,42 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import axios from 'axios';
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import axios from "axios";
 import { useState, useEffect } from "react";
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 const defaultExpenses = [
-  {name: "TV/GSM", description: "Test", value: "80"},
-  {name: "PET", description: "My pet expenses for the month", value: "135"},
-  {name: "APARTMENT", description: "Monthly fee (LOAN) for the apartment", value: "765"},
-  {name: "WATER/ENERGY", description: "Water consumption and Energy", value: "258"},
-  {name: "GARAGE RENT", description: "Garage fee for parking the car", value: "120"},
-  {name: "COSMETICS", description: "Monthly expenses for cosmetics stuffs", value: "75"},
+  { name: "TV/GSM", description: "Test", value: "80" },
+  { name: "PET", description: "My pet expenses for the month", value: "135" },
+  {
+    name: "APARTMENT",
+    description: "Monthly fee (LOAN) for the apartment",
+    value: "765",
+  },
+  {
+    name: "WATER/ENERGY",
+    description: "Water consumption and Energy",
+    value: "258",
+  },
+  {
+    name: "GARAGE RENT",
+    description: "Garage fee for parking the car",
+    value: "120",
+  },
+  {
+    name: "COSMETICS",
+    description: "Monthly expenses for cosmetics stuffs",
+    value: "75",
+  },
 ];
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   width: "10em",
   height: "6em",
   color: theme.palette.text.secondary,
@@ -39,7 +55,7 @@ const ExpensesDirectionStack = () => {
           let uniqueArray = Array.from(uniqueSet).map(JSON.parse);
           setExpenses(uniqueArray);
         } else {
-          console.log("Something is wrong")
+          console.log("Something is wrong");
         }
       } catch (err) {
         //console.log(err); TODO makes tests fail because of network delay response
@@ -54,22 +70,35 @@ const ExpensesDirectionStack = () => {
 
   return (
     <Stack
-      direction={{ xs: 'column', sm: 'row' }}
+      direction={{ xs: "column", sm: "row" }}
       spacing={{ xs: 1, sm: 2, md: 2 }}
     >
       {expenses.map((expense) => {
-        return <Item>
-          <React.Fragment>
-            <Tooltip title={expense.description} placement="top">
-              <Typography component="p" align="left" color="orange" variant="standard">
-                {expense.name}
+        return (
+          <Item>
+            <React.Fragment>
+              <Tooltip title={expense.description} placement="top">
+                <Typography
+                  component="p"
+                  align="left"
+                  color="orange"
+                  variant="standard"
+                >
+                  {expense.name}
+                </Typography>
+              </Tooltip>
+              <Typography
+                sx={{ mt: 1 }}
+                component="p"
+                color="grey"
+                align="left"
+                variant="h5"
+              >
+                $ {expense.value}
               </Typography>
-            </Tooltip>
-            <Typography sx={{ mt: 1 }} component="p" color="black" align="left" variant="h5">
-              $ {expense.value}
-            </Typography>
-          </React.Fragment>
-        </Item>;
+            </React.Fragment>
+          </Item>
+        );
       })}
     </Stack>
   );
--- frontend/src/pages/home/Dashboard.js
+++ frontend/src/pages/home/Dashboard.js
@@ -3,23 +3,27 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 
 //import Chart from "./Chart";
-import Orders from "../../components/dynamic/Orders";
+import Chip from "@mui/material/Chip";
 import Incomes from "../../components/dynamic/Incomes";
 import Copyright from "../components/Copyright";
 import Navbar from "../components/NavBar";
 import Sidebar from "../components/Sidebar";
 import ExpensesDirectionStack from "../../components/dynamic/ExpensesStack";
-import Divider from '@mui/material/Divider';
+import SavingsDirectionStack from "../../components/dynamic/SavingsStack";
+import Divider from "@mui/material/Divider";
 
 console.log("Dashboard.js loaded. Loading functions...");
 
-const mdTheme = createTheme();
+const defaultTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function Dashboard() {
   console.log("DashboardContent function loaded.");
@@ -30,7 +34,7 @@ function Dashboard() {
 
   const sidebarWidth = 240;
   return (
-    <ThemeProvider theme={mdTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
 
@@ -50,18 +54,90 @@ function Dashboard() {
             backgroundColor: (theme) =>
               theme.palette.mode === "light"
                 ? theme.palette.grey[200]
-                : theme.palette.grey[900],
+                : theme.palette.grey[1000],
             flexGrow: 1,
             height: "100vh",
+            paddingTop: "5em",
             overflow: "auto",
           }}
         >
-          <Toolbar />
-          <Container maxWidth sx={{ mt: 2, mb: 2 }}>
-            <ExpensesDirectionStack/>
+          <Divider>
+            <Chip
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[200]
+                    : theme.palette.grey[900],
+              }}
+              label="EXPENSES"
+            />
+          </Divider>
+          {/* Expense Stack */}
+          <Container sx={{ mt: 2, mb: 4 }}>
+            <ExpensesDirectionStack />
           </Container>
-          <Divider/>
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+
+          {/* Incomes Stack */}
+          <Divider>
+            <Chip
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[200]
+                    : theme.palette.grey[900],
+              }}
+              label="INCOME"
+            />
+          </Divider>
+          <Container sx={{ mt: 2, mb: 4 }}>
+            {/* Incomes */}
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Incomes />
+              </Paper>
+            </Grid>
+            {/* Recent Orders */}
+            {/* <Grid item xs={12}>
+              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                <Orders />
+              </Paper>
+            </Grid>*/}
+          </Container>
+          <Divider>
+            <Chip
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[200]
+                    : theme.palette.grey[900],
+              }}
+              label="SAVINGS"
+            />
+          </Divider>
+          {/* Savings Stack */}
+          <Container sx={{ mt: 2, mb: 4 }}>
+            <SavingsDirectionStack />
+          </Container>
+
+          <Divider>
+            <Chip
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[200]
+                    : theme.palette.grey[900],
+              }}
+              label="STATS"
+            />
+          </Divider>
+          {/* Statistics Stack */}
+          <Container maxWidth="lg" sx={{ mt: 2, mb: 4 }}>
             <Grid container spacing={3}>
               {/* Chart */}
               <Grid item xs={12} md={8} lg={9}>
@@ -74,25 +150,7 @@ function Dashboard() {
                   }}
                 >
                   {/* <Chart /> */}
-                </Paper>
-              </Grid>
-              {/* Incomes */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                >
-                  <Incomes />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Orders />
+                  Chart holder
                 </Paper>
               </Grid>
             </Grid>
