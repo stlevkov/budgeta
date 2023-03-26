4439af0b8739d136fa36167e54aa2a0e0fcfd179
stlevkov <stlevkov@gmail.com>
2023-03-26 02:07:15 +0200

    #85 allExpenses aligned with ExpensesContext

--- frontend/src/contexts/ExpensesState.jsx
+++ frontend/src/contexts/ExpensesState.jsx
@@ -4,20 +4,30 @@ export default class ExpensesState {
   constructor() {
     this.state = [];
     this.listeners = [];
+    this.sumExpenses = undefined;  // TODO make them private with #
 
     getExpenses().then((data) => {
       this.setState(data);
     });
   }
 
+  /**
+   * Set the new state. Also calculates the sumExpenses. Use getSumExpenses to get the calculated sum.
+   * @param newState expenses array
+   */
   setState(newState) {
     console.log("[ExpensesState] Setting the state to new state...", newState);
+    this.sumExpenses = newState.map(newState => newState.value).reduce((a, b) => a + b);
     this.state = [...newState];
     this.listeners.forEach((listener) => listener(this.state)); // Notify all listeners that the state has changed
   }
 
   getState() {
     return this.state;
+  }
+
+  getSumExpenses(){
+    return this.sumExpenses;
   }
 
   addListener(listener) {
